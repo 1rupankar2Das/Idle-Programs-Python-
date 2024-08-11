3de936dca682c205f03c6dcbee5b3d6eccf4c1f1
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2024-08-11 18:01:23 +0530

    Add files via upload

---
+++ Prog12.py
@@ -0,0 +1,26 @@
+a = float(16/(float(3)))
+print(a)
+
+a=format(float(16/(float(3))),'.2f')
+print(a)
+
+a=format(3**50,'.5e')
+print(a)
+
+a=format(123456,',')
+print(a)
+
+a=10+7
+print(a)
+
+a=(50+40-35)
+print(a)
+
+a=(12+10)
+print(a)
+
+a=(96/12)
+print(a)
+
+a=(-30*4)+500
+print(a)
