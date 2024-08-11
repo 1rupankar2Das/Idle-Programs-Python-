af935569b574674330262ae07f06d751d6e39df1
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2024-08-11 18:01:41 +0530

    Add files via upload

---
+++ Prog13.py
@@ -0,0 +1,36 @@
+a = 5*3.0
+print(a)
+
+a = 19+3.5
+print(a)
+
+a = 78//5
+print(a)
+
+a = 78%5
+print(a)
+
+a = 152.78%3.0
+print(a)
+
+a = 5**3
+print(a)
+
+a = 121**0.5
+print(a)
+
+a = '''Good morning everyone.
+    "welcome to the world of 'Python'."
+    Happy reading.'''
+print(a)
+
+a = 'Hello'
+print(a)
+
+print('what\'s your name?')
+
+print("Hello All. \t Welcome to the world of Python.")
+
+a = ("I have studied many programming languages. \
+But my best favourite language is python.")
+print(a)
